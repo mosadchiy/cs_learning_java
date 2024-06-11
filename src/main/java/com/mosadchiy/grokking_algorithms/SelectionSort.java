cdb5e63978618f3e2ede2e08951532a693e1158e
Mikhail Osadchiy <lava1804.92@gmail.com>
2024-06-10 20:10:36 -0400

    Updated SelectionSort

--- src/main/java/com/mosadchiy/grokking_algorithms/SelectionSort.java
+++ src/main/java/com/mosadchiy/grokking_algorithms/SelectionSort.java
@@ -1,30 +1,31 @@
 package com.mosadchiy.grokking_algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int[] array = {5, 3, 6, 2, 10};
-        System.out.println(Arrays.toString(selectionSort(array)));
+        List<Integer> array = new ArrayList<>(Arrays.asList(5, 3, 6, 2, 10));
+        System.out.println(selectionSort(array));
     }
 
-    private static int[] selectionSort(int[] array) {
-        int[] sortedArray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            int smallest = findSmallest(array);
-            sortedArray[i] = array[smallest];
-            array[smallest] = Integer.MAX_VALUE;
+    private static List<Integer> selectionSort(List<Integer> array) {
+        List<Integer> sortedArray = new ArrayList<>();
+        while (array.size() > 0) {
+            int smallestIndex = findSmallestIndex(array);
+            sortedArray.add(array.remove(smallestIndex));
         }
         return sortedArray;
     }
 
-    private static int findSmallest(int[] array) {
-        int smallest = array[0];
+    private static int findSmallestIndex(List<Integer> array) {
+        int smallest = array.get(0);
         int smallestIndex = 0;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < smallest) {
-                smallest = array[i];
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i) < smallest) {
+                smallest = array.get(i);
                 smallestIndex = i;
             }
         }
