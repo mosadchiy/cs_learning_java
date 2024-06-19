f8586f8970dbabdea28d23b95407a3273e4cf7fa
Mikhail Osadchiy <lava1804.92@gmail.com>
2024-06-19 15:42:34 -0400

    simplified RecursionBinarySearch

--- src/main/java/com/mosadchiy/grokking_algorithms/RecursionBinarySearch.java
+++ src/main/java/com/mosadchiy/grokking_algorithms/RecursionBinarySearch.java
@@ -7,29 +7,23 @@ import static com.mosadchiy.grokking_algorithms.BinarySearch.generateSortedArray
 public class RecursionBinarySearch {
 
     public static void main(String[] args) {
-        int[] array = generateSortedArray(10);
-        int item = 4;
+        int[] array = generateSortedArray(5);
+        int item = 3;
         System.out.println("input array: " + Arrays.toString(array));
         System.out.println("item to find: " + item);
         findItemInArrayBinarySearch(array, item);
     }
 
     private static void findItemInArrayBinarySearch(int[] array, int item) {
-        if (array.length == 0) {
+        if (array.length == 1) {
+            if (array[0] == item) {
+                System.out.println("Item found: " + item);
+                return;
+            }
             return;
         }
-        int lowIndex = 0;
-        int highIndex = array.length - 1;
-        int midIndex = (lowIndex + highIndex) / 2;
-        int arrayItem = array[midIndex];
-        if (arrayItem == item) {
-            System.out.println("Item found: " + item);
-            return;
-        }
-        if (arrayItem > item) {
-            findItemInArrayBinarySearch(Arrays.copyOfRange(array, lowIndex, midIndex), item);
-        } else {
-            findItemInArrayBinarySearch(Arrays.copyOfRange(array, midIndex + 1, highIndex + 1), item);
-        }
+        int midIndex = (array.length - 1) / 2;
+        findItemInArrayBinarySearch(Arrays.copyOfRange(array, 0, midIndex + 1), item);
+        findItemInArrayBinarySearch(Arrays.copyOfRange(array, midIndex + 1, array.length), item);
     }
 }
